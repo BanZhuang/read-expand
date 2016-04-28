4f0bce985669259e4e5819921bd2d5aa448e946b
weimulin <mulin.wei@bblink.cn>
2016-04-28 08:00:25 +0800

    04.28

---
+++ app/src/main/java/io/github/w4mxl/read/ui/login/LoginMvpView.java
@@ -0,0 +1,13 @@
+package io.github.w4mxl.read.ui.login;
+
+import io.github.w4mxl.read.ui.base.MvpView;
+
+/**
+ * @author: wml
+ * @date : 2016/03/15 15:04
+ * @desc : TODO
+ */
+public interface LoginMvpView extends MvpView {
+
+    void showSignInSuccessful();
+}
---
+++ app/src/main/java/io/github/w4mxl/read/ui/login/LoginPresenter.java
@@ -0,0 +1,17 @@
+package io.github.w4mxl.read.ui.login;
+
+import io.github.w4mxl.read.ui.base.BasePresenter;
+
+/**
+ * @author: wml
+ * @date : 2016/03/15 15:05
+ * @desc : TODO
+ */
+public class LoginPresenter extends BasePresenter<LoginMvpView> {
+
+    public void signin() {
+
+    }
+}
+
+
